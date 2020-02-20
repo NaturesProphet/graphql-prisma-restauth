c4e873e4444fb4afc1ff32a09f2ebc31bdfa11cc
Mateus Garcia <mateus.gigainfo@gmail.com>
2020-02-20 14:57:50 -0300

    feat(GraphQl): graphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphqlOptions } from './graphql/options/graphql.options';
 
 @Module( {
-  imports: [],
-  controllers: [],
+  imports: [
+    GraphQLModule.forRootAsync( {
+      useClass: GraphqlOptions,
+    } ),
+  ],
   providers: [],
 } )
 export class AppModule { }
