dcd1baef6f5420f256c04fd6ab4bede76e3d0291
Mateus Garcia <mateus.gigainfo@gmail.com>
2020-02-20 16:18:55 -0300

    chore(prisma) ajuste

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '../../prisma/generated/prisma-client';
+import { Prisma } from './client/generated/prisma-client';
 
 @Injectable()
 export class PrismaService {
